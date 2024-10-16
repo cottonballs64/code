c1433cdfd333f79367be52d49ecccb6906f144ae
cottonballs64 <micah.stradling@gmail.com>
2024-10-16 10:24:07 -0500

    fixing old ticket

--- school/week7/MicahS-10022024-exitTicket.cpp
+++ school/week7/MicahS-10022024-exitTicket.cpp
@@ -5,25 +5,28 @@ Write a function called echo that takes input from a user and prints it back out
 */
 #include <iostream>
 using namespace std;
-string text;
 
-string getText(){
-    cout << "Enter any text\n";
-    getline(cin, text);
-    return text;
-}
+string userText;
+string getText();
 
-void printText(string text){
-    cout << text << endl;
-}
+void printText(string inputText);
 
-void echo(){
-    getText();
-    printText(text);
-}
+void echo();
 
 int main() {
 echo ();
 return 0;
+}
 
+string getText(){
+    string newText;
+    cout << "Enter any text\n";
+    getline(cin, newText);
+    return newText;
+}
+void printText(string inputText){
+    cout << inputText << endl;
+}
+void echo(){
+    printText(getText());
 }
