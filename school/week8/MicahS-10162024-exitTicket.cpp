c1240e2cc8514e996f356378162b0ba7e7ff1882
cottonballs64 <micah.stradling@gmail.com>
2024-10-16 10:30:28 -0500

    new exit ticket

---
+++ school/week8/MicahS-10162024-exitTicket.cpp
@@ -0,0 +1,32 @@
+/*
+Driver's License ExamDriver's License Exam
+
+The local Driver's License Office has asked you to write a program
+that grades the written portion of the driver's license exam.
+
+The exam has 20 multiple-choice questions. 
+
+Here are the correct answers(in order of question):
+A, D, B, B, C, B, A, B, C, D, A, C, D, B, D, C, C, A, D, B
+
+Your program should store the correct answers shown above in an array.
+
+It should ask the user to enter the student's answers for each of the 20 questions,
+and the answers should be stored in another array. 
+
+After the student's answers have been entered,
+the program should display a message indicating whether the student passed or failed the exam.
+(A student must correctly answer 15 of the 20 questions to pass the exam.)
+
+It should then display the total number of correctly answered questions,
+the total number of incorrectly answered questions,
+and a list showing the question numbers of the incorrectly answered questions.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+return 0;
+
+}
